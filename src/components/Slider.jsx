d1f96fea80ff8f038e6c25112332f2dd67f3978a
Jay panchal <jay.panchal0704@gmail.com>
2023-03-27 18:58:03 +0530

    initial project setup

---
+++ src/components/Slider.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
+import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+`;
+
+const Slider = () => {
+  return (
+    <Container>
+      <Arrow>
+        <ArrowLeftOutlinedIcon />
+      </Arrow>
+      <Arrow>
+        <ArrowRightOutlinedIcon />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
